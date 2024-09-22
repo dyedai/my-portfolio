f1cb7a792c26a6be76cf47eed4129264ef4074c2
DaikiKakimi <4250ikiad@gmail.com>
2024-09-23 01:59:04 +0900

    アニメーションレスポンシブ対応

--- src/app/animatedlink.tsx
+++ src/app/animatedlink.tsx
@@ -1,5 +1,21 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import Link from "next/link";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState<boolean>(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    setMatches(media.matches);
+
+    const listener = () => setMatches(media.matches);
+    media.addListener(listener);
+
+    return () => media.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
 
 interface AnimatedLinkProps {
   href: string;
@@ -11,9 +27,19 @@ interface AnimatedLinkProps {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const AnimatedLink: FC<AnimatedLinkProps> = ({ href, english, japanese, className }) => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   const [hovered, setHovered] = useState(false);
   return (
-    <Link href={href} className="relative inline-block text-2xl font-semibold text-white" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+    <Link
+      href={href}
+      className="relative inline-block text-2xl font-semibold text-white"
+      onMouseEnter={() => {
+        if (isDesktop) setHovered(true);
+      }}
+      onMouseLeave={() => {
+        if (isDesktop) setHovered(false);
+      }}
+    >
       <span className={`transition-opacity duration-300 ${hovered ? "opacity-0" : "opacity-100"}`}>{english}</span>
       <span className={`absolute transition-opacity duration-300 ${hovered ? "opacity-100" : "opacity-0"}`}>{japanese}</span>
     </Link>
