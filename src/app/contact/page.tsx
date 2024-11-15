4789c1c7f157de1356a8553fa74b416c57aba547
DaikiKakimi <4250ikiad@gmail.com>
2024-11-15 19:39:32 +0900

    contact編集

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,8 +5,146 @@ export default function ContactPage() {
   console.log("このページではないな");
 
   return (
-    <div className="text-center">
-      <div className="flex items-center gap-20 justify-center">
+    <div className="block">
+      <div className="mt-8 grid grid-cols-1 md:grid-cols-5 md:mt-0 lg:grid-cols-8 lg:mt-0 gap-20">
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://twitter.com/___daidai__"} target="_blank">
+          <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
+        </Link>
+        <Link href={"https://github.com/dyedai"} target="_blank">
+          <Image src="/images/github-mark-white.svg" alt="x-logo-white" width={100} height={100} />
+        </Link>
         <Link href={"https://twitter.com/___daidai__"} target="_blank">
           <Image src="/images/logo-white.png" alt="x-logo-white" width={100} height={100} />
         </Link>
