65339075d0fa77ef47260fa55dde457262057e53
DaikiKakimi <4250ikiad@gmail.com>
2024-11-17 05:06:09 +0900

    githubのリンク修正

--- src/app/works/bookreview/page.tsx
+++ src/app/works/bookreview/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "../../glow.css";
 
 export default function BookreviewPage() {
   console.log("隠し要素はどこかな？？");
@@ -6,8 +7,10 @@ export default function BookreviewPage() {
   return (
     <div>
       <div className="flex h-screen w-full flex-col lg:flex-row">
-        <div className="lg:w-1/2 h-ful text-white lg:px-8 pt-2">
-          <h1 className="font-dot text-3xl lg:text-4xl my-8 text-center">&lt;BookReview/&gt;</h1>
+        <div className="lg:w-1/2 h-ful text-white lg:px-8 pt-2 rainbow-text">
+          <h1 className="font-dot text-3xl lg:text-4xl my-8 text-center">
+            &lt;BookReview/&gt;
+          </h1>
           <p className="mx-2">
             BookReviewは、ユーザーが書籍をレビューし、他のユーザーが投稿したレビューを簡単に参照できるアプリです。このアプリは、書籍選びに迷ったときに参考になるレビューを提供し、他の読者と感想を共有できる場を作ることを目的としています。開発の動機としては、自分が読んだ本の感想を他の人と共有したいという思いと、他の人が読んだ本のレビューを参考にして新しい本を見つけたいという考えからです。
             このアプリの特徴は、シンプルで直感的なUIデザインと、Tailwind
@@ -15,28 +18,86 @@ export default function BookreviewPage() {
             このように、ユーザーフレンドリーな設計にこだわり、誰でも直感的に操作できる環境を整えることに注力しています。
           </p>
           <p className="mx-2 mt-2">
-            BookReview is an application that allows users to review books and easily refer to reviews posted by other users. The purpose of this application is to provide users with reviews that they
-            can refer to when they are unsure about their choice of books and to create a place where they can share their impressions with other readers. The motivation behind the development of this
-            application was the desire to share my impressions of books I have read with others and to find new books by referring to reviews of books that others have read. The app features a simple
-            and intuitive UI design and a visually appealing and easy-to-use interface using Tailwind CSS and daisyUI. Users are also free to customize their own profiles and have the ability to post,
-            edit, and delete reviews of books. Users can easily refer to other users&apos; reviews, which can help them in finding new books. As you can see, our focus is on user-friendly design and
+            BookReview is an application that allows users to review books and
+            easily refer to reviews posted by other users. The purpose of this
+            application is to provide users with reviews that they can refer to
+            when they are unsure about their choice of books and to create a
+            place where they can share their impressions with other readers. The
+            motivation behind the development of this application was the desire
+            to share my impressions of books I have read with others and to find
+            new books by referring to reviews of books that others have read.
+            The app features a simple and intuitive UI design and a visually
+            appealing and easy-to-use interface using Tailwind CSS and daisyUI.
+            Users are also free to customize their own profiles and have the
+            ability to post, edit, and delete reviews of books. Users can easily
+            refer to other users&apos; reviews, which can help them in finding
+            new books. As you can see, our focus is on user-friendly design and
             creating an environment that anyone can operate intuitively.
           </p>
-          <p className="mx-2 my-4">仕様技術：React, Vite, Prettier, Eslint, TailwindCss, daisyUI, Pines, compressor.js</p>
-          <a className="underline  text-blue-600 mx-2" href="https://github.com/dyedai/react-bookreview" target="_blank">
+          <p className="mx-2 my-4">
+            仕様技術：React, Vite, Prettier, Eslint, TailwindCss, daisyUI,
+            Pines, compressor.js
+          </p>
+          <a
+            className="underline  text-blue-600 mx-2 text-2xl"
+            href="https://github.com/dyedai/react-bookreview"
+            target="_blank"
+          >
             GitHub
           </a>
         </div>
 
         <div className="lg:w-1/2 h-full lg:overflow-y-scroll">
           <div className="grid grid-cols-1 gap-4 p-4">
-            <Image src="/images/bookreview/bookhome.png" alt="bookhome" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/bookreview/bookpost.png" alt="bookpost" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/bookreview/bookdetail.png" alt="bookdetail" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/bookreview/bookuser.png" alt="bookuser" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/bookreview/booksignup.png" alt="booksignup" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/bookreview/booklogin.png" alt="booklogin" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/bookreview/bookredit.png" alt="bookedit" className="w-full h-auto" width={2400} height={1600} />
+            <Image
+              src="/images/bookreview/bookhome.png"
+              alt="bookhome"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/bookreview/bookpost.png"
+              alt="bookpost"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/bookreview/bookdetail.png"
+              alt="bookdetail"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/bookreview/bookuser.png"
+              alt="bookuser"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/bookreview/booksignup.png"
+              alt="booksignup"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/bookreview/booklogin.png"
+              alt="booklogin"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/bookreview/bookredit.png"
+              alt="bookedit"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
           </div>
         </div>
       </div>
--- src/app/works/luggpal/page.tsx
+++ src/app/works/luggpal/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "../../glow.css";
 
 export default function ReceiptbatllePage() {
   console.log("隠し要素はどこかな？？");
@@ -6,8 +7,10 @@ export default function ReceiptbatllePage() {
   return (
     <div>
       <div className="flex h-screen w-full flex-col lg:flex-row">
-        <div className="lg:w-1/2 h-ful text-white lg:px-8 pt-2">
-          <h1 className="font-dot text-3xl lg:text-4xl my-8 text-center">&lt;LuggPal/&gt;</h1>
+        <div className="lg:w-1/2 h-ful text-white lg:px-8 pt-2 rainbow-text">
+          <h1 className="font-dot text-3xl lg:text-4xl my-8 text-center">
+            &lt;LuggPal/&gt;
+          </h1>
           <p className="mx-2">
             LuggPalは、荷物を預けたい人（Traveler）と荷物を預かりたい人（Keeper）をマッチングさせるサービスです。
             荷物を預ける側は、地域や日時をもとに効率的に預かり手を探すことができ、預かる側は、余ったスペースを活用して荷物を預けたい人とマッチングできます。
@@ -17,28 +20,82 @@ export default function ReceiptbatllePage() {
             このように、旅行者とスペースの提供者双方のニーズを満たすサービスを目指しています。
           </p>
           <p className="mx-2 mt-2">
-            LuggPal is a service that matches people who want to leave their luggage (Traveler) with people who want to keep their luggage (Keeper). Luggages can be stored in a location or on a date
-            and time basis, and keepers can match up with people who want to store their luggage in their spare space. This service is intended to relieve the burden of carrying large luggage and
-            suitcases, especially when traveling far away from home or on vacation. There are times during a trip when people find it inconvenient to carry heavy luggage up stairs or through narrow
-            streets, and we thought that if they could leave their luggage at a place of their choice, they would be able to enjoy their trip more comfortably. On the other hand, we also responded to
-            the need for efficient use of space, and if there is enough space in a house or store, we can earn revenue by using that space to store luggage. In this way, we aim to provide a service
+            LuggPal is a service that matches people who want to leave their
+            luggage (Traveler) with people who want to keep their luggage
+            (Keeper). Luggages can be stored in a location or on a date and time
+            basis, and keepers can match up with people who want to store their
+            luggage in their spare space. This service is intended to relieve
+            the burden of carrying large luggage and suitcases, especially when
+            traveling far away from home or on vacation. There are times during
+            a trip when people find it inconvenient to carry heavy luggage up
+            stairs or through narrow streets, and we thought that if they could
+            leave their luggage at a place of their choice, they would be able
+            to enjoy their trip more comfortably. On the other hand, we also
+            responded to the need for efficient use of space, and if there is
+            enough space in a house or store, we can earn revenue by using that
+            space to store luggage. In this way, we aim to provide a service
             that satisfies the needs of both travelers and space providers.
           </p>
-          <p className="mx-2 mt-4">仕様技術：Next.js, Prettier, Eslint, Firebase, Figma</p>
+          <p className="mx-2 mt-4">
+            仕様技術：Next.js, Prettier, Eslint, Firebase, Figma
+          </p>
           <p className="mb-4 mx-2">JPhacks出場作品</p>
-          <a className="underline  text-blue-600 mx-2" href="https://github.com/jphacks/tk_2422" target="_blank">
+          <a
+            className="underline  text-blue-600 mx-2 text-2xl"
+            href="https://github.com/jphacks/tk_2422"
+            target="_blank"
+          >
             GitHub
           </a>
         </div>
 
         <div className="lg:w-1/2 h-full lg:overflow-y-scroll">
           <div className="grid grid-cols-1 gap-4 p-4">
-            <Image src="/images/luggpal/login.png" alt="login" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/luggpal/home.png" alt="home" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/luggpal/home2.png" alt="home2" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/luggpal/keeper.png" alt="keeper" className="w-full h-auto" width={2400} height={1600} />
-            <Image src="/images/luggpal/registrar.png" alt="registar" className="w-full h-auto" width={2400} height={1600} />
-            <video src="/images/luggpal/demo.mp4" controls autoPlay loop playsInline className="w-full h-auto" width="2400" height="1600" />
+            <Image
+              src="/images/luggpal/login.png"
+              alt="login"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/luggpal/home.png"
+              alt="home"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/luggpal/home2.png"
+              alt="home2"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/luggpal/keeper.png"
+              alt="keeper"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <Image
+              src="/images/luggpal/registrar.png"
+              alt="registar"
+              className="w-full h-auto"
+              width={2400}
+              height={1600}
+            />
+            <video
+              src="/images/luggpal/demo.mp4"
+              controls
+              autoPlay
+              loop
+              playsInline
+              className="w-full h-auto"
+              width="2400"
+              height="1600"
+            />
           </div>
         </div>
       </div>
